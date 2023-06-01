c16d0d1e85f3669556ba59adbb168f25ae2d384e
kayaarynee <110926254+kayaarynee@users.noreply.github.com>
2023-06-02 02:11:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+let changeThemeButtons = document.querySelectorAll('.change-theme');
+
+
+changeThemeButtons.forEach(button => {
+  button.addEventListener('click', function () {
+    let theme = this.dataset.theme;
+    applyTheme(theme);
+  });
+});
+
+function applyTheme(themeName) {
+  document.querySelector('[title="theme"]').setAttribute('href', `css/theme-${themeName}.css`);
+  changeThemeButtons.forEach(button => {
+    button.style.display = 'block';
+  });
+  document.querySelector(`[data-theme="${themeName}"]`).style.display = 'none';
+}
+
+function toggleText() {
+  let button = document.getElementById('font-button-sans-serif');
+  let text = document.querySelector('body');
+  console.log(button)
+  if (button.innerHTML == 'С засечками') {
+    button.innerHTML = "Без засечек";
+    text.style.fontFamily = 'Lora, serif';
+  } else {
+    button.innerHTML = "С засечками";
+    text.style.fontFamily = 'Monserrat, sans-serif';
+  }
+}
+
+function increaseFontSize() {
+  var parent = document.querySelector('body');
+  var currentSize = parseFloat(window.getComputedStyle(parent)['font-size']);
+  parent.style.fontSize = (currentSize + 1) + 'px';
+  var headers = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+  for (var i = 0; i < headers.length; i++) {
+    var newSize = parseFloat(window.getComputedStyle(headers[i])['font-size']) + 1;
+    headers[i].style.fontSize = newSize + 'px';
+  }
+}
+
+function decreaseFontSize() {
+  var parent = document.querySelector('body');
+  var currentSize = parseFloat(window.getComputedStyle(parent)['font-size']);
+  parent.style.fontSize = (currentSize - 1) + 'px';
+  var headers = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+  for (var i = 0; i < headers.length; i++) {
+    var newSize = parseFloat(window.getComputedStyle(headers[i])['font-size']) - 1;
+    headers[i].style.fontSize = newSize + 'px';
+  }
+}
+
+function resetFontSize() {
+  var parent = document.querySelector('body');
+  var initSize = 16;
+  parent.style.fontSize = initSize + 'px';
+  var headers = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+  for (var i = 0; i < headers.length; i++) {
+    headers[i].style.fontSize = '';
+  }
+} 
